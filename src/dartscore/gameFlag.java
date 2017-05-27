3de69a5399bee0c42de7dbcb42dbae282765310b
Joseph Polen <joseph.polen12@gmail.com>
2017-05-27 14:55:14 -0400

    Initial commit.

---
+++ src/dartscore/gameFlag.java
@@ -0,0 +1,16 @@
+/*
+ *                         DartScore
+ *                 A DartScoring application by
+ *                        Joseph Polen
+ */
+package dartscore;
+
+/**
+ *
+ * @author Joe
+ */
+public class gameFlag {
+
+    static int gameType = 1;
+
+}
